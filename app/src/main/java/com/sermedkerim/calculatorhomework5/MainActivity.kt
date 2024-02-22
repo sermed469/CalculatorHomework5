8d8575ca098ff22be17bda1c33cf93027f7ea9d2
sermed469 <sermet537@gmail.com>
2024-02-22 17:59:27 +0300

    App design is changed

--- app/src/main/java/com/sermedkerim/calculatorhomework5/MainActivity.kt
+++ app/src/main/java/com/sermedkerim/calculatorhomework5/MainActivity.kt
@@ -9,7 +9,6 @@ import com.sermedkerim.calculatorhomework5.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
